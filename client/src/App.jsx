1374bc59472fa3b804a6c818cc7b0ed06cb2441c
Robi Nurhidayat <robivanpersie290899@gmail.com>
2023-05-03 20:03:40 +0700

    create file Dashboard,Error,index,Register dan mendaftarkan komponent ini ke routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Landing from "./pages/Landing";
-import styled from "styled-components";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Dashboard, Error, Register } from "./pages";
 
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,6 +1,7 @@
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
 import { Logo } from "../components";
+import { Link } from "react-router-dom";
 
 function Landing() {
   return (
@@ -23,7 +24,9 @@ function Landing() {
             irony retro dreamcatcher echo park cardigan shabby chic subway tile.
           </p>
 
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
