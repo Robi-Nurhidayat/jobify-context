d5e09e8762aec1beb2be19750f71cbd12124814e
Robi Nurhidayat <robivanpersie290899@gmail.com>
2023-05-19 20:20:27 +0700

    jwt

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,8 +1,30 @@
-import { json } from "express";
+import { StatusCodes } from "http-status-codes";
 
 const errorHandlerMiddlerware = (err, req, res, next) => {
-  console.log(err);
-  res.status(500).json({ msg: err });
+  console.log(err.message);
+
+  const defaultError = {
+    StatusCodes: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong, try again later",
+  };
+
+  if (err.name === "ValidationError") {
+    // defaultError.StatusCodes = StatusCodes.BAD_REQUEST;
+    // defaultError.msg = err.message;
+
+    defaultError.StatusCodes = StatusCodes.BAD_REQUEST;
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+  }
+
+  if (err.code && err.code === 11000) {
+    defaultError.StatusCodes = StatusCodes.BAD_REQUEST;
+    defaultError.msg = "Field has to be unique";
+  }
+
+
+  res.status(defaultError.StatusCodes).json({ msg: defaultError.msg });
 };
 
 export default errorHandlerMiddlerware;
