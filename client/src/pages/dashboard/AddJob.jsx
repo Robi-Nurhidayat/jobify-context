4e42d7bd790d7deb474eb86a218e55b12c49677c
Robi Nurhidayat <robivanpersie290899@gmail.com>
2023-06-06 18:21:44 +0700

    working with job

--- client/src/pages/dashboard/AddJob.jsx
+++ client/src/pages/dashboard/AddJob.jsx
@@ -1,7 +1,84 @@
-import React from "react";
+import { FormRow, Alert } from "../../components";
+import Wrapper from "../../assets/wrappers/DashboardFormPage";
+import { useAppContext } from "../../context/appContext";
 
 function AddJob() {
-  return <div>AddJob Page</div>;
+  const {
+    isEditing,
+    showAlert,
+    displayAlert,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+  } = useAppContext();
+
+  const handleJobInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    console.log(`${name}: ${value}`);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!position || !company || !jobLocation) {
+      displayAlert();
+      return;
+    }
+  };
+  return (
+    <Wrapper>
+      <form className="form">
+        <h3>{isEditing ? "Edit job" : "Add job"}</h3>
+        {showAlert && <Alert />}
+
+        <div className="form-center">
+          {/* position */}
+          <FormRow
+            type="text"
+            name="position"
+            value={position}
+            handleChange={handleJobInput}
+          />
+
+          {/* company */}
+          <FormRow
+            type="text"
+            name="company"
+            value={company}
+            handleChange={handleJobInput}
+          />
+
+          {/* jobLocation */}
+          <FormRow
+            type="text"
+            labelText="Job location"
+            name="jobLocation"
+            value={jobLocation}
+            handleChange={handleJobInput}
+          />
+
+          {/* job type */}
+          {/* job status */}
+
+          <div className="btn-container">
+            <button
+              type="submit"
+              className="btn btn-block submit-btn"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </Wrapper>
+  );
 }
 
 export default AddJob;
