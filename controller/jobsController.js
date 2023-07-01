aeb2e93839f55f4961218583c741458cc95ffd0d
Robi Nurhidayat <robivanpersie290899@gmail.com>
2023-07-01 08:36:27 +0700

    membuat pagination di sisi backend

--- controller/jobsController.js
+++ controller/jobsController.js
@@ -61,10 +61,17 @@ const getAllJob = async (req, res) => {
     result = result.sort("-position");
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 1;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result;
-  res
-    .status(StatusCodes.OK)
-    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 const updateJob = async (req, res) => {
   const id = req.params.id;
